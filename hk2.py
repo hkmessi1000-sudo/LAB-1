f72a37bda0d63804f6cdf6ddf69fd0f9f758b1ad
hkmessi1000-sudo <hkmessi1000@gmail.com>
2026-04-21 14:15:56 +0530

    Add files via upload

---
+++ hk2.py
@@ -0,0 +1,104 @@
+#Question 1
+'''
+a="Hello world"
+print(a)
+
+'''
+#Question 2
+'''
+a=10
+b=5
+print(a+b)
+
+a=10
+b=5
+print(a-b)
+
+a=10
+b=5
+print(a*b)
+
+a=10
+b=5
+print(a/b)
+
+
+a=10
+b=5
+print(a%b)
+
+'''
+#Question 3
+'''
+a=3
+print(a**0.5)
+
+'''
+#Question 4
+'''
+a=4
+b=7
+print("area of the triangle : ",0.5*a*b)
+
+'''
+#Question 5
+'''
+a=3
+b=2
+print(a**2+b**2+2*a*b)
+
+a=3
+b=2
+print(a**2+b**2-2*a*b)
+
+a=3
+b=4
+print((a-b)*(a+b))
+
+'''
+#Question 7
+'''
+kilometer = 8
+print("Miles :",kilometer*0.63) 
+
+'''
+#Question 8
+'''
+celsius = 6
+Fahrenheit = (celsius*1.8)+32
+print("Fahrenheit :",Fahrenheit)
+
+'''
+#Question 9
+'''
+a=24
+print(a%10)
+
+'''
+#Question 10
+'''
+a=345
+print(a%100)
+
+'''
+#Question 11
+'''
+integer = 54623
+print("input an integer :",integer)
+print("square of 6 is :",6**2)
+
+'''
+#Question 6
+'''
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping: a =", a, "b =", b)
+
+temp = a
+a = b
+b = temp
+
+'''
+
+print("After swapping: a =", a, "b =", b)
