182513100a32416a77fef1719a8fcd64411986a0
hkmessi1000-sudo <hkmessi1000@gmail.com>
2026-04-24 14:29:21 +0530

    Add files via upload

---
+++ hk3.py
@@ -0,0 +1,189 @@
+#Question 1 
+'''
+a=2
+b=3
+if a<b:
+     print(a)
+else :
+    print(b)
+
+'''
+#Question 2
+'''
+a=3
+b=4
+if a>b:
+    print(a)
+else:
+    print(b)
+
+'''
+#Qusetion 3
+'''
+num=int(input("enter a number :"))
+if(num>0):
+    print("absolute:",num)
+else:
+    print("absolute:",-(num))
+
+'''
+#Question 4
+'''
+a=int(input("enter a number :"))
+if a%10==0:
+    print("yes")
+else:
+    print("no")
+
+'''
+#Question 5
+'''
+a=int(input("num="))
+if a%15==0:
+         print("multiple of 5")
+else:
+    print("not multiple of 5")
+    
+'''
+#Question 6
+'''
+a=int(input("num:"))
+if a%30==0:
+    print("multiple of 10")
+else:
+    print("not multiple of 10")
+
+'''
+#Question 7
+'''
+num=11
+if((num>10 and num<99)or(num>-10 and num<-99)):
+    print("It is two digit number")
+else:
+    print("It is not a two digit number")
+
+'''
+#Question 8
+'''
+num=345
+if((num>100 and num<999)or(num>-100 and num<-999)):
+    print("It is a three digit number")
+else:
+    print("It is not a three digit number")
+
+'''
+#Question 9
+'''
+num=190
+if(num%10==0):
+    print("It ends with 0")
+else:
+    print("It not ends with 0")
+
+'''
+#Question 10
+'''
+num=int(input("enter a number:"))
+square=num**2
+print("square root:",square)
+if("square>=50"):
+    print("above 50")
+else:
+    print("Below 50")    
+
+'''
+#Question 11
+'''
+num1=int(input("enter a number :"))
+num2=int(input("enter a number :"))
+differ=num1-num2
+if(differ==0):
+    print("The difference is 0")
+else:
+    print("The difference is not 0")
+
+'''
+#Question 12
+'''
+mark=int(input("Enter Mark:"))
+if(mark>=50 and mark<=100):
+         print("Passed")
+elif(mark<50):
+    print("Fail")
+else:
+    print("Invalid Input")
+
+'''
+#Question 13
+'''
+num=int(input("Enter number:"))
+if(num%10==0):
+        print("yes")
+else:
+    print("No")
+
+'''
+#Question 14
+'''
+num=int(input("Enter number two digit:"))
+h=num//10
+k=num%10
+if(h>k):
+        print("Biggest digit :",h)
+else:
+    print("Biggest digit:",k)
+
+'''
+#Question 15
+'''
+choice=int(input("give Choice:"))
+if(choice==1):
+    print("The exam will be easy")
+elif(choice==2):
+    print("The exam will be difficult")
+else:
+    print("Invalid Input")
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
