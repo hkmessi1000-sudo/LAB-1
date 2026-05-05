5bb3bb497e8c4f5e1751dc535444a52c174f0abf
hkmessi1000-sudo <hkmessi1000@gmail.com>
2026-05-05 14:30:06 +0530

    Add files via upload

---
+++ LAB 3 ANSWERS.py
@@ -0,0 +1,30 @@
+#Question 1
+'''
+n=int(input("enter a number: "))
+for i in range (1,n+1,1):
+    print(i,end=" ")
+
+'''
+#Question 2
+'''
+n=int(input("enter a number: "))
+for i in range(1,n+6,1):
+    if i%2==0:
+        print(i,end=" ")
+
+'''
+#Question 3
+'''
+n=int(input("enter a number: "))
+for i in range(1,n+6,2):
+    if i%2!=0:
+        print(i,end=" ")
+
+'''
+#Question 4
+'''
+n=int(input("enter a number: "))
+for i in range(3,n+15,3):
+    print(i,end=" ")
+
+'''
